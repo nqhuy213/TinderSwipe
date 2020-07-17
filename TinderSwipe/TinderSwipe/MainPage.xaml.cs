0f5dee4d9a9d46e8b8dc3da9512801ac3e1ce61f
Huy <nqhuy213@gmail.com>
2020-07-17 15:00:02 +1000

    Views Created

--- TinderSwipe/TinderSwipe/MainPage.xaml.cs
+++ TinderSwipe/TinderSwipe/MainPage.xaml.cs
@@ -17,7 +17,40 @@ namespace TinderSwipe
         public MainPage()
         {
             InitializeComponent();
-            MainGrid.Children.Add(new SwiperControl());
+            AddInitialPhotos();
+        }
+        private void AddInitialPhotos()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                InsertPhoto();
+            }
+        }
+        private void InsertPhoto()
+        {
+            var photo = new SwiperControl();
+            photo.OnDislike += Handle_OnDislike;
+            photo.OnLike += Handle_OnLike;
+            this.MainGrid.Children.Insert(0, photo);
+        }
+        private int _likeCount;
+        private int _denyCount;
+        private void UpdateGui()
+        {
+            likeLabel.Text = _likeCount.ToString();
+            denyLabel.Text = _denyCount.ToString();
+        }
+        private void Handle_OnLike(object sender, EventArgs e)
+        {
+            _likeCount++;
+            InsertPhoto();
+            UpdateGui();
+        }
+        private void Handle_OnDislike(object sender, EventArgs e)
+        {
+            _denyCount++;
+            InsertPhoto();
+            UpdateGui();
         }
     }
 }
