e623ac46e4ad9eaa177728c5a3a018b5535c45a6
Huy <nqhuy213@gmail.com>
2020-07-17 10:45:08 +1000

    Define SwiperControl

---
+++ TinderSwipe/TinderSwipe/Controls/SwiperControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinderSwipe.Utils;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TinderSwipe.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwiperControl : ContentView
+    {
+        public SwiperControl()
+        {
+            InitializeComponent();
+            var picture = new Picture();
+            Photo.Source = new UriImageSource() { Uri = picture.Uri };
+            DescriptionLabel.Text = picture.Description;
+        }
+    }
+}
