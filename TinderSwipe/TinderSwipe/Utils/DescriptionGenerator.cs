a4f4db5d9d6803b8cb728059b4c61a7464315bb7
Huy <nqhuy213@gmail.com>
2020-07-17 10:27:54 +1000

    Add Picture, DescriptionGenerator Utils

---
+++ TinderSwipe/TinderSwipe/Utils/DescriptionGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinderSwipe.Utils
+{
+    public class DescriptionGenerator
+    {
+        private string[] _adjectives = { "nice", "great", "horrible", "powerful", "pretty", "old", "brand new" };
+        private string[] _other = { "picture of grandpa", "car", "photo of a forest", "duck", "dog" };
+        private static Random random = new Random();
+
+        public string Generate()
+        {
+            var a = _adjectives[random.Next(_adjectives.Count())];
+            var b = _other[random.Next(_other.Count())];
+            return $"A {a} {b}";
+        }
+    }
+}
